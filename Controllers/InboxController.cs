1a3b0859118e5939ff1511620ab37d87a76ca8ec
jawadSaleem <jawadsaleem932@gmail.com>
2025-05-16 20:01:28 +0500

    did common changes for managing inbox!

---
+++ Controllers/InboxController.cs
@@ -0,0 +1,43 @@
+﻿using HopIn_Server.Models;
+using HopIn_Server.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HopIn_Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class InboxController : ControllerBase
+	{
+		private readonly InboxService _inboxService;
+		public InboxController(InboxService inboxService) => _inboxService = inboxService;
+
+		private IActionResult ApiResponse(int statusCode, bool success, string? message = null, string? errorMsg = null, object? data = null)
+		{
+			var response = new
+			{
+				success,
+				message,
+				errorMsg,
+				data
+			};
+			return StatusCode(statusCode, response);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> createInbox(Inbox inbox)
+		{
+			try {
+				var result = await _inboxService.createInbox(inbox);
+				if (!result.success) {
+					return ApiResponse(400,result.success,result.message);
+				}
+				return ApiResponse(200, result.success, result.message);
+			}
+			catch (Exception e) {
+				return ApiResponse(500, false, "An unexpected error occurred.", errorMsg: e.Message);
+			}
+		}
+
+	}
+}
---
+++ Services/MessagingService.cs
@@ -0,0 +1,47 @@
+﻿using HopIn_Server.Configurations;
+using HopIn_Server.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace HopIn_Server.Services
+{
+	public class MessagingService
+	{
+		private readonly IMongoCollection<Message> _messagesCollection;
+
+		public MessagingService(IOptions<DbSettings> databaseSettings) {
+			var mongoClient = new MongoClient(databaseSettings.Value.connecitonString);
+			var dbName = mongoClient.GetDatabase(databaseSettings.Value.dbName);
+			_messagesCollection = dbName.GetCollection<Message>(databaseSettings.Value.collectionNames["messagesColl"]);
+		}
+		//action methods
+		public async Task<(bool success, string message)> AddMessage(Message message) {
+			if (string.IsNullOrEmpty(message.senderId) ||
+				string.IsNullOrEmpty(message.content) )
+			{
+				return (false,"Please provide required fields!");
+			}
+			//checking if message already exists
+			var incomingMsg = await _messagesCollection.Find(x => x.msgId == message.msgId).FirstOrDefaultAsync();
+			if (incomingMsg != null) {
+				return (false,"This message already exists!");
+			}
+			await _messagesCollection.InsertOneAsync(message);
+			return (true, "Message sent!");
+		}
+
+		public async Task<(bool success, string message)> DeleteMsg(string msgId ) {
+			if (string.IsNullOrWhiteSpace(msgId)) {
+				return (false,"Message ID cant be empty!");
+			}
+			//checking if message exists
+			var incomingMessage = await _messagesCollection.Find(x => x.msgId == msgId).FirstOrDefaultAsync();
+			if (incomingMessage == null) {
+				return (false, "No such message exists!");
+			}
+			await _messagesCollection.DeleteOneAsync(x => x.msgId == msgId);
+			return (true, "Message Deleted!");
+		}
+
+	}
+}
